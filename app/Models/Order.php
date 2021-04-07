4dfa3b39e9be9fd72dc6c8feb686bd0b92009020
Alejandro <alejandroc@boxcode.com.mx>
2021-04-07 16:32:43 -0500

    Se espesificó el nombre de las tablas que usará cada modelo y se agregó el atributo fillable en cada modelo indicando los campos de cada tabla

--- app/Models/Order.php
+++ app/Models/Order.php
@@ -8,4 +8,13 @@ use Illuminate\Database\Eloquent\Model;
 class Order extends Model
 {
     use HasFactory;
+
+    protected $table="PurchaseOrder";
+
+    protected $fillable=[
+        'idUser',
+        'idProduct',
+        'completed',
+        'date'
+    ];
 }
