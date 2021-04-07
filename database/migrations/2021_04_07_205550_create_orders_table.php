bcca9a13b6c594c1b6e4e5dfc7c99fee64d6f90c
Alejandro <alejandroc@boxcode.com.mx>
2021-04-07 16:30:35 -0500

    Se actualizó la tabla PurchaseOrder donde se agregó el atributo completed y date para conocer las ordenes de compra que se completaron y en que fecha se hicieron

--- database/migrations/2021_04_07_205550_create_orders_table.php
+++ database/migrations/2021_04_07_205550_create_orders_table.php
@@ -13,12 +13,14 @@ class CreateOrdersTable extends Migration
      */
     public function up()
     {
-        Schema::create('Order', function (Blueprint $table) {
+        Schema::create('PurchaseOrder', function (Blueprint $table) {
             $table->id();
             $table->bigInteger('idUser')->unsigned();
             $table->foreign('idUser')->references('id')->on('User')->onDelete('cascade');
             $table->bigInteger('idProduct')->unsigned();
             $table->foreign('idProduct')->references('id')->on('Product')->onDelete('cascade');
+            $table->char('completed',1);
+            $table->date('date');
             $table->timestamps();
         });
     }
@@ -30,6 +32,6 @@ class CreateOrdersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('Order');
+        Schema::dropIfExists('PurchaseOrder');
     }
 }
